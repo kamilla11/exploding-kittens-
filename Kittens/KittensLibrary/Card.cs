27de86438b77f7c20567af740fe93d10867e8a56
Kuchina Olga <olmenua2003@gmail.com>
2022-12-23 22:12:10 +0300

    [add] add kitten library

---
+++ Kittens/KittensLibrary/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace KittensLibrary;
+
+public class Card : CardType
+{
+    public string Img { get; set; }
+}
+
+public class CardType
+{
+    public CardName CardName { get; set; }
+    public string? Icon { get; set; }
+    public string? Description { get; set; }
+}
+
+public enum CardName
+{
+    Attack,
+    Defuse,
+    Nope,
+    Shuffle,
+    Skip,
+    Kitten,
+    ExplodingKitten,
+    SeeTheFuture,
+    Favor
+}
